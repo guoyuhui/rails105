e85e230d43bb301764fb9328ac7223c9239bedfb
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-02-09 01:22:22 +0800

    implement pre_4_chapter

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,49 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+    redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "update successfully"
+    else
+      render :edit
+    end
+  end
+
+    def destroy
+      @group = Group.find(params[:id])
+      @group.destroy
+      flash[:alert] = "Group deleted"
+      redirect_to groups_path
+    end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
 end
