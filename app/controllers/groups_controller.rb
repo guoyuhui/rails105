fe1c1e5bc0f987cc6336d8e994d86c3241bb8bd1
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-02-09 14:34:49 +0800

    ride show_page edit button

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
   def index
     @groups = Group.all
   end
@@ -24,11 +26,10 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+
   end
 
   def update
-    @group = Group.find(params[:id])
 
     if @group.update(group_params)
       redirect_to groups_path, notice: "update successfully"
@@ -38,13 +39,21 @@ class GroupsController < ApplicationController
   end
 
     def destroy
-      @group = Group.find(params[:id])
+
       @group.destroy
       flash[:alert] = "Group deleted"
       redirect_to groups_path
     end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
